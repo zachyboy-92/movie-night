ac789ca615760b0658e348550e701266695aa60b
zachyboy-92 <zakaria_0456@hotmail.com>
2021-08-22 16:53:48 -0400

    first commit

---
+++ javascript/home.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector(".hamburger");
+const navlist = document.querySelector("#navbar");
+
+hamburger.addEventListener("click", () => {
+  navlist.classList.toggle("show");
+});
